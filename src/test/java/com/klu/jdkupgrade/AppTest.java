774a279e1f7f1e1f26bea705ef3fc189f073a9f1
kl2400090205 <240009205@kluniversity@gmail.com>
2026-03-16 15:44:38 +0530

    Added project files

---
+++ src/test/java/com/klu/jdkupgrade/AppTest.java
@@ -0,0 +1,12 @@
+package com.klu.jdkupgrade;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
